b0213bd27874b435c37712f759226625b1bfed3e
Reece Bedding <reecebedding@hotmail.co.uk>
2015-04-23 14:32:43 +0100

    Personalised header

---
+++ VORBS/Utils/CustomHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace VORBS.Utils
+{
+    public class CustomHelpers
+    {
+        public static string GetUserNameFromDomain(string userName)
+        {
+            string domainSection = ConfigurationManager.AppSettings["domain"].ToString();
+            string shortName = userName.Replace(domainSection, "");
+            return shortName;
+        }
+    }
+}
