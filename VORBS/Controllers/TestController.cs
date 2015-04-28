4de131014c46b0eb44a5efd0e77c359ed9f2b464
Reece Bedding <reecebedding@hotmail.co.uk>
2015-04-28 13:11:19 +0100

    Added framwork for deciding UI from mockups

---
+++ VORBS/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VORBS.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult UI_1()
+        {
+            return View();
+        }
+
+        public ActionResult UI_2()
+        {
+            return View();
+        }
+
+        public ActionResult UI_3()
+        {
+            return View();
+        }
+    }
+}
