81d42563b7ef404141e670d59e5e8636c17191c5
Reece Bedding <reecebedding@hotmail.co.uk>
2015-05-08 10:55:14 +0100

    Added bookings to room DTO

--- VORBS/Models/DTOs/RoomDTO.cs
+++ VORBS/Models/DTOs/RoomDTO.cs
@@ -8,12 +8,13 @@ namespace VORBS.Models.DTOs
     public class RoomDTO
     {
         public int ID { get; set; }
-
+        
         public string RoomName { get; set; }
         public int ComputerCount { get; set; }
         public int PhoneCount { get; set; }
         public int SeatCount { get; set; }
         public bool SmartRoom { get; set; }
 
+        public virtual IEnumerable<BookingDTO> Bookings { get; set; }
     }
 }
