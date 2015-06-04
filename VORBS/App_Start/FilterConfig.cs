4b56b483df367f6d705ebe62f28fdff5cb2f4f71
Reece Bedding <reecebedding@hotmail.co.uk>
2015-06-04 13:50:30 +0100

    Added display of version to UI

--- VORBS/App_Start/FilterConfig.cs
+++ VORBS/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Web;
 using System.Web.Mvc;
 
 namespace VORBS
@@ -8,6 +9,15 @@ namespace VORBS
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new VersionNumberInjection());
+        }
+    }
+
+    public class VersionNumberInjection : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.CurrentVersion = ConfigurationManager.AppSettings["version"];
         }
     }
 }
