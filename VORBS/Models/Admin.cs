d995ea597b433d9438dfd1875ddbf517d6ec56ab
Jatin Sidhpura <VOAITDEV\9993692>
2015-06-04 11:02:49 +0100

    Version 0.3.11 - RB13

---
+++ VORBS/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VORBS.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string pId { get; set; }
+        public string Username { get; set; }
+        public int PermissionLevel { get; set; }
+    }
+}
---
+++ VORBS/Models/DTOs/AdminDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VORBS.Models.DTOs
+{
+    public class AdminDTO
+    {
+        public int Id { get; set; }
+        public string pId { get; set; }
+        public string Username { get; set; }
+        public int PermissionLevel { get; set; }
+    }
+}
