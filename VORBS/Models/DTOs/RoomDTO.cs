4261b2537b8a3282408dc421397d9a085fedc980
Jatin Sidhpura <VOAITDEV\9993692>
2015-06-09 11:49:08 +0100

    RB-14 (Partial Done Not Version)

--- VORBS/Models/DTOs/RoomDTO.cs
+++ VORBS/Models/DTOs/RoomDTO.cs
@@ -8,13 +8,14 @@ namespace VORBS.Models.DTOs
     public class RoomDTO
     {
         public int ID { get; set; }
-        
+     
         public string RoomName { get; set; }
         public int ComputerCount { get; set; }
         public int PhoneCount { get; set; }
         public int SeatCount { get; set; }
         public bool SmartRoom { get; set; }
 
+        public virtual IEnumerable<RoomDTO> Rooms { get; set; }
         public virtual IEnumerable<BookingDTO> Bookings { get; set; }
     }
 }
