ecdc3a4efec69556a14cdc8d156023e84ccda608
William Voyle <VOAITDEV\7220451>
2015-06-17 13:18:02 +0100

    Version 0.4.16 - RB-284 - Edit Modal Attendees Numeric Validation fix

--- VORBS/Scripts/Shared Functions/bookingFunctions.js
+++ VORBS/Scripts/Shared Functions/bookingFunctions.js
@@ -215,7 +215,7 @@ $(document).ready(function () {
         initval: 1
     });
 
-    $("#attendeesControl,.phoneNoControl").keydown(function (e) {
+    $("#attendeesControl,.phoneNoControl,#attendees").keydown(function (e) {
         // Allow: backspace, delete, tab, escape, enter and .
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
             // Allow: Ctrl+A, Command+A
