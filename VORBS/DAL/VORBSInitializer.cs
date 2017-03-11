0b9abb1f1848f80f805b4a2586debfb7962168b2
Reece Bedding <reecebedding@hotmail.co.uk>
2017-03-11 12:15:11 +0000

    Simplified seed data

--- VORBS/DAL/VORBSInitializer.cs
+++ VORBS/DAL/VORBSInitializer.cs
@@ -14,9 +14,7 @@ namespace VORBS.DAL
         {
             var locations = new List<Location>()
             {
-                new Location(){Name="Location1", Active= true },
-                new Location(){Name="Location2", Active= true },
-                new Location(){Name="Location3", Active= true },
+                new Location(){ Name="Location1", Active= true }
             };
 
             locations.ForEach(l => context.Locations.Add(l));
@@ -24,12 +22,7 @@ namespace VORBS.DAL
 
             var admins = new List<Admin>()
             {
-                new Admin(){ ID = 1, FirstName = "Admin1", LastName = "Admin1", LocationID = 1, Email = "fakeemail1@mail.com", PermissionLevel = 2, PID = "0000001" },
-                new Admin(){ ID = 2, FirstName = "Admin2", LastName = "Admin2", LocationID = 1, Email = "fakeemail2@mail.com", PermissionLevel = 2, PID = "0000002" },
-                new Admin(){ ID = 3, FirstName = "Admin3", LastName = "Admin3", LocationID = 1, Email = "fakeemail3@mail.com", PermissionLevel = 2, PID = "0000003" },
-                new Admin(){ ID = 4, FirstName = "Admin4", LastName = "Admin4", LocationID = 1, Email = "fakeemail4@mail.com", PermissionLevel = 2, PID = "0000004" },
-                new Admin(){ ID = 5, FirstName = "Admin5", LastName = "Admin5", LocationID = 1, Email = "fakeemail5@mail.com", PermissionLevel = 2, PID = "0000005" },
-                new Admin(){ ID = 5, FirstName = "Admin6", LastName = "Admin6", LocationID = 1, Email = "fakeemail6@mail.com", PermissionLevel = 2, PID = "0000006" }
+                new Admin(){ ID = 1, FirstName = "Admin1", LastName = "Admin1", LocationID = 1, Email = "fakeemail1@mail.com", PermissionLevel = 2, PID = "0000001" }
             };
 
             admins.ForEach(a => context.Admins.Add(a));
@@ -37,47 +30,7 @@ namespace VORBS.DAL
 
             var rooms = new List<Room>()
             {
-                new Room(){ LocationID =  1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 8, SmartRoom = false,Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 0, PhoneCount = 1, SeatCount = 16, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 16, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 7, PhoneCount = 1, SeatCount = 16, SmartRoom = false, Active = true }
-                
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 4, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 4, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 10, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 8, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 4, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 10, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 6, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 10, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 0, PhoneCount = 1, SeatCount = 9, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 8, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 8, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 1, PhoneCount = 1, SeatCount = 12, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 1, RoomName = "room-name", ComputerCount = 0, PhoneCount = 1, SeatCount = 12, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 6, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 3, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 3, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 6, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 3, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 3, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 24, SmartRoom = true, Active = true  }
-
-                ,new Room(){ LocationID = 2, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 24, SmartRoom = true, Active = true  }
-
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 12, PhoneCount = 0, SeatCount = 12, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 18, SmartRoom = true, Active = true }
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 6, SmartRoom = false, Active = true }
-
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 1, PhoneCount = 0, SeatCount = 8, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 0, PhoneCount = 0, SeatCount = 8, SmartRoom = false, Active = true }
-                ,new Room(){ LocationID = 3, RoomName = "room-name", ComputerCount = 1, PhoneCount = 0, SeatCount = 4, SmartRoom = false, Active = true }
+                new Room(){ LocationID =  1, RoomName = "Room1", ComputerCount = 1, PhoneCount = 1, SeatCount = 8, SmartRoom = false, Active = true }
             };
 
             rooms.ForEach(r => context.Rooms.Add(r));
@@ -85,46 +38,7 @@ namespace VORBS.DAL
 
             var bookings = new List<Booking>()
             {
-                new Booking(){ RoomID = 1, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 2, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 3, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 4, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 6, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 7, Owner = "User2", StartDate = new DateTime(2015, 5, 13, 10, 0, 0), EndDate = new DateTime(2015, 5, 13, 14, 0, 0) }
-                ,new Booking(){ RoomID = 8, Owner = "Admin2 Admin2", StartDate = new DateTime(2015, 5, 13, 10, 30, 0), EndDate = new DateTime(2015, 5, 13, 14, 0, 0) }
-                ,new Booking(){ RoomID = 9, Owner = "User4", StartDate = new DateTime(2015, 5, 13, 11, 0, 0), EndDate = new DateTime(2015, 5, 13, 12, 30, 0) }
-                ,new Booking(){ RoomID = 10, Owner = "User5", StartDate = new DateTime(2015, 5, 13, 11, 0, 0), EndDate = new DateTime(2015, 5, 13, 12, 30, 0) }
-                ,new Booking(){ RoomID = 11, Owner = "User6", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 12, Owner = "User6", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 16, 30, 0) }
-                ,new Booking(){ RoomID = 13, Owner = "User5", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 14, Owner = "User7", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 15, Owner = "User5", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 16, Owner = "User6", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 17, Owner = "User6", StartDate = new DateTime(2015, 5, 13, 9, 0, 0), EndDate = new DateTime(2015, 5, 13, 17, 0, 0) }
-                ,new Booking(){ RoomID = 10, Owner = "User5", StartDate = new DateTime(2015, 5, 13, 15, 0, 0), EndDate = new DateTime(2015, 5, 13, 15, 30, 0) }
-                ,new Booking(){ RoomID = 18, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 13, 11, 00, 0), EndDate = new DateTime(2015, 5, 13, 12, 30, 0) }
-                ,new Booking(){ RoomID = 18, Owner = "User4", StartDate = new DateTime(2015, 5, 13, 13, 00, 0), EndDate = new DateTime(2015, 5, 13, 16, 30, 0) }
-                ,new Booking(){ RoomID = 19, Owner = "User4", StartDate = new DateTime(2015, 5, 13, 10, 00, 0), EndDate = new DateTime(2015, 5, 13, 12, 30, 0) }
-                ,new Booking(){ RoomID = 19, Owner = "User6", StartDate = new DateTime(2015, 5, 13, 13, 00, 0), EndDate = new DateTime(2015, 5, 13, 14, 30, 0) }
-                ,new Booking(){ RoomID = 19, Owner = "User8", StartDate = new DateTime(2015, 5, 13, 15, 00, 0), EndDate = new DateTime(2015, 5, 13, 15, 30, 0) }
-                ,new Booking(){ RoomID = 20, Owner = "User4", StartDate = new DateTime(2015, 5, 13, 09, 00, 0), EndDate = new DateTime(2015, 5, 13, 17, 00, 0) }
 
-                ,new Booking(){ RoomID = 2, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 3, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 4, Owner = "Admin3 Admin3", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 7, Owner = "User7", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 9, Owner = "User2", StartDate = new DateTime(2015, 5, 28, 11, 30, 0), EndDate = new DateTime(2015, 5, 28, 13, 30, 0) }
-                ,new Booking(){ RoomID = 9, Owner = "User4", StartDate = new DateTime(2015, 5, 28, 14, 0, 0), EndDate = new DateTime(2015, 5, 28, 16, 30, 0) }
-                ,new Booking(){ RoomID = 10, Owner = "User4", StartDate = new DateTime(2015, 5, 28, 10, 30, 0), EndDate = new DateTime(2015, 5, 28, 11, 00, 0) }
-                ,new Booking(){ RoomID = 12, Owner = "Admin2 Admin2", StartDate = new DateTime(2015, 5, 28, 13, 30, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 14, Owner = "User7", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 15, Owner = "Admin2 Admin2", StartDate = new DateTime(2015, 5, 28, 13, 0, 0), EndDate = new DateTime(2015, 5, 28, 14, 30, 0) }
-                ,new Booking(){ RoomID = 16, Owner = "User6", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 17, Owner = "User6", StartDate = new DateTime(2015, 5, 28, 9, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 18, Owner = "User2", StartDate = new DateTime(2015, 5, 28, 10, 30, 0), EndDate = new DateTime(2015, 5, 28, 11, 30, 0) }
-                ,new Booking(){ RoomID = 18, Owner = "User5", StartDate = new DateTime(2015, 5, 28, 12, 0, 0), EndDate = new DateTime(2015, 5, 28, 17, 0, 0) }
-                ,new Booking(){ RoomID = 19, Owner = "User4", StartDate = new DateTime(2015, 5, 28, 10, 30, 0), EndDate = new DateTime(2015, 5, 28, 12, 30, 0) }
-                ,new Booking(){ RoomID = 20, Owner = "User4", StartDate = new DateTime(2015, 5, 28, 09, 00, 0), EndDate = new DateTime(2015, 5, 28, 17, 00, 0) }
             };
 
             int dayOffset = 1;
