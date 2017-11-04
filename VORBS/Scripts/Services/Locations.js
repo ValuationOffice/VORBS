2416b0a57b45a950112b2c375f5297bf7d0c8f7d
Charlie Lynch <lynchcoding@gmail.com>
2017-11-04 13:14:15 +0000

    Added Angular locations service with specs.

---
+++ VORBS/Scripts/Services/Locations.js
@@ -0,0 +1,45 @@
+﻿(function () {
+
+    angular.module('vorbs.services')
+        .factory('LocationsService', LocationsService);
+
+    LocationsService.$inject = ['$resource'];
+
+    function LocationsService($resource) {
+        return $resource('/api/locations', {},
+            {
+                query: {
+                    method: 'GET',
+                    isArray: true
+                },
+                getByID: {
+                    method: 'GET',
+                    params: {
+                        id: '@id'
+                    },
+                    url: '/api/locations/:id'
+                },
+                getByStatus: {
+                    method: 'GET',
+                    isArray: true,
+                    params: {
+                        status: '@status',
+                        extraInfo: '@extraInfo'
+                    },
+                    url: '/api/locations/:status/:extraInfo'
+                },
+                create: {
+                    method: 'POST'
+                },
+                update: {
+                    method: 'PUT',
+                    params: {
+                        locationId: '@locationId',
+                        status: '@status'
+                    },
+                    url: '/api/locations/:locationId/:status'
+                }
+            });
+    }
+
+})();
---
+++ VORBS/Scripts/Tests/Data/locationsMockData.js
@@ -0,0 +1,72 @@
+﻿
+var locationMockData = (function () {
+    return {
+        getMockLocations: getMockLocations,
+        getSingleMockLocation: getMockLocations()[0]
+    };
+
+    function getMockLocations() {
+        return [{
+            "id": 1,
+            "name": "Location1",
+            "rooms": [{
+                "id": 1,
+                "roomName": "Room1",
+                "computerCount": 8,
+                "phoneCount": 1,
+                "seatCount": 8,
+                "smartRoom": false,
+                "active": true,
+                "location": null,
+                "rooms": null,
+                "bookings": null
+            },
+            {
+                "id": 2,
+                "roomName": "Room_2",
+                "computerCount": 2,
+                "phoneCount": 2,
+                "seatCount": 6,
+                "smartRoom": false,
+                "active": true,
+                "location": null,
+                "rooms": null,
+                "bookings": null
+            }],
+            "locationCredentials": [],
+            "active": true,
+            "additionalInformation": null
+        }, {
+            "id": 2,
+            "name": "Location2",
+            "rooms": [{
+                "id": 1,
+                "roomName": "POD 1",
+                "computerCount": 1,
+                "phoneCount": 1,
+                "seatCount": 5,
+                "smartRoom": false,
+                "active": true,
+                "location": null,
+                "rooms": null,
+                "bookings": null
+            },
+            {
+                "id": 2,
+                "roomName": "POD 2",
+                "computerCount": 0,
+                "phoneCount": 1,
+                "seatCount": 3,
+                "smartRoom": false,
+                "active": true,
+                "location": null,
+                "rooms": null,
+                "bookings": null
+            }],
+            "locationCredentials": [],
+            "active": true,
+            "additionalInformation": null
+        }];
+    }
+})();
+
