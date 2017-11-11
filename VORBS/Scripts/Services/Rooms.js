9b06da3101ddc3e4ddb397b808cfe445465d3e19
Charlie Lynch <lynchcoding@gmail.com>
2017-11-11 19:35:44 +0000

    Added Angular rooms service with specs

---
+++ VORBS/Scripts/Services/Rooms.js
@@ -0,0 +1,59 @@
+﻿(function () {
+
+    angular.module('vorbs.services')
+        .factory('RoomsService', RoomsService);
+
+    RoomsService.$inject = ['$resource'];
+
+    function RoomsService($resource) {
+        return $resource('/api/room', {}, {
+            query: {
+                method: 'GET',
+                isArray: true
+            },
+            getByID: {
+                method: 'GET',
+                params: {
+                    id: '@id'
+                },
+                url: '/api/room/:id'
+            },
+            getByStatus: {
+                method: 'GET',
+                isArray: true,
+                params: {
+                    locationName: '@locationName',
+                    status: '@status'
+                },
+                url: '/api/room/:locationName/:status'
+            },
+            getByName: {
+                method: 'GET',
+                params: {
+                    locationId: '@locationId',
+                    roomName: '@roomName'
+                }, 
+                url: '/api/room/:locationId/:roomName'
+            },
+            create: {
+                method: 'POST'
+            },
+            update: {
+                method: 'PUT',
+                params: {
+                    id: '@id'
+                },
+                url: '/api/room/:id'
+            },
+            updateStatus: {
+                method: 'PATCH',
+                params: {
+                    id: '@id',
+                    status: '@status'
+                },
+                url: '/api/room/:id/:status'
+            }
+        });
+    }
+
+})();
