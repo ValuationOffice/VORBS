f1cc5e25e695f22aad1550d767dc80b77f034074
Charlie Lynch <lynchcoding@gmail.com>
2018-02-06 22:49:48 +0000

    Add interceptor function to Locations service to sort locations by name
    - Remove now redundant orderBy from newBookings view

--- VORBS/Scripts/Services/Locations.js
+++ VORBS/Scripts/Services/Locations.js
@@ -6,11 +6,37 @@
     LocationsService.$inject = ['$resource'];
 
     function LocationsService($resource) {
+
+        function sortLocationByName(locations) {
+
+            var locationArray = locations || [];
+
+            if (locationArray.length > 0) {
+                locationArray = locationArray.sort(function (a, b) {
+                    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+                });
+            }
+
+            return locationArray;
+        }
+
         return $resource('/api/locations', {},
             {
                 query: {
                     method: 'GET',
-                    isArray: true
+                    isArray: true,
+                    interceptor: {
+                        response: function (resp) {
+
+                            var data = resp.data;
+
+                            if (data) {
+                                data = sortLocationByName(data);
+                            }
+
+                            return data;
+                        }
+                    }
                 },
                 getByID: {
                     method: 'GET',
@@ -25,6 +51,18 @@
                     params: {
                         status: '@status',
                         extraInfo: '@extraInfo'
+                    },
+                    interceptor: {
+                        response: function (resp) {
+
+                            var data = resp.data;
+
+                            if (data) {
+                                data = sortLocationByName(data);
+                            }
+
+                            return data;
+                        }
                     },
                     url: '/api/locations/:status/:extraInfo'
                 },
