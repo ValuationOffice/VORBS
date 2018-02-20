09aee5ef27afedd7adb7f637d5ddfa49bfeed29c
Reece Bedding <reecebedding@hotmail.co.uk>
2018-02-20 16:57:57 +0000

    Add deploy script that includes bower components

---
+++ VORBS/deploy.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    [string]$OutDir = "$HOME\Desktop\VORBS_DEPLOY"
+)
+
+bower install
+
+msbuild .\VORBS.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=True /p:PublishUrl=$OutDir
