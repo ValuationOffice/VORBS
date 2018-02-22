46ba556454cbb94455931a2a9716522d40ba0eb7
Charlie Lynch <lynchcoding@gmail.com>
2018-02-22 20:48:15 +0000

    Add Angular admins service with specs

---
+++ VORBS/Scripts/Services/Admins.js
@@ -0,0 +1,16 @@
+﻿(function () {
+
+    angular.module('vorbs.services')
+        .factory('AdminsService', AdminsService);
+
+    AdminsService.$inject = ['$resource'];
+
+    function AdminsService($resource) {
+        return $resource('/api/admin/:id', { id: '@id' }, {
+            update: {
+                method: 'PUT'
+            }
+        });
+    }
+
+})();
---
+++ VORBS/Scripts/Tests/Data/adminMockData.js
@@ -0,0 +1,26 @@
+﻿var adminMockData = (function () {
+
+    return {
+        getAdminUser: getAdminUser
+    };
+
+    function getAdminUser() {
+        return {
+            "id": 1,
+            "pid": "0000001",
+            "firstName": "Dean",
+            "lastName": "Martin",
+            "location": {
+                "id": 1,
+                "name": "Location1",
+                "rooms": null,
+                "locationCredentials": null,
+                "active": true,
+                "additionalInformation": null
+            },
+            "email": "fakeemail1@mail.com",
+            "permissionLevel": 2
+        }
+    }
+
+})();
