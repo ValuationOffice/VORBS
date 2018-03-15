e233803c7b7014055c0d36d5d704456b0347e2a1
Reece Bedding <reecebedding@hotmail.co.uk>
2018-03-15 19:54:53 +0000

    Fix issue with creating linked resources for emails

---
+++ VORBS.Tests/Utils/EmailClientTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Net.Mail;
+using VORBS.Utils;
+using VORBS.Utils.interfaces;
+
+namespace VORBS.Tests
+{
+    [TestFixture]
+    public class EmailClientTests
+    {
+        [Test]
+        public void ShouldRetrieveLinkedResource()
+        {
+            ISmtpClient smtpClient = new EmailClient();
+            var imageSource = $@"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Utils\Images\govuklogo.png";
+            var imageId = "someid";
+
+            byte[] expectedBytes = StreamExtensions.GetBytesFromFile(imageSource);
+            
+            LinkedResource resource = smtpClient.GetLinkedResource(imageSource, imageId);
+            byte[] actualBytes = resource.ContentStream.ReadAllBytes();
+
+            Assert.AreEqual(imageId, resource.ContentId);
+            Assert.AreEqual(expectedBytes, actualBytes);
+        } 
+    }
+
+    internal static class StreamExtensions
+    {
+        public static byte[] GetBytesFromFile(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                return fs.ReadAllBytes();
+            }
+        }
+
+        public static byte[] ReadAllBytes(this Stream instream)
+        {
+            MemoryStream stream = null;
+
+            if (instream is MemoryStream)
+                stream = ((MemoryStream)instream);
+            else
+            {
+                stream = new MemoryStream();
+                instream.CopyTo(stream);
+            }
+
+            using (stream)
+            {   
+                byte[] bytes = stream.ToArray();
+                stream.Close();
+                stream.Dispose();
+                return bytes;
+            }
+        }
+    }
+}
