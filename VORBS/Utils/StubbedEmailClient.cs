a68626df0464e7654de5b3d3a1dc251fad0af561
Reece Bedding <reecebedding@hotmail.co.uk>
2018-03-27 13:53:38 +0100

    Use Smtp PickUp delivery for stubbing saving of emails locally

--- VORBS/Utils/StubbedEmailClient.cs
+++ VORBS/Utils/StubbedEmailClient.cs
@@ -30,28 +30,15 @@ namespace VORBS.Utils
 
         public void Send(MailMessage message)
         {
+            SmtpClient client = new SmtpClient("stubbedhostname");
+            client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+
             string rootDirectory = $@"{AppDomain.CurrentDomain.SetupInformation.ApplicationBase}Logs/{DateTime.Now.ToString("yyyy-MM-dd")}/EmailLogs/{message.To.FirstOrDefault().User}";
             if (!Directory.Exists(rootDirectory))
                 Directory.CreateDirectory(rootDirectory);
 
-            string fileName = $"{message.Subject}_{DateTime.Now.ToString("hh-mm-ss")}";
-            using (FileStream fs = File.Create($"{rootDirectory}/{fileName}.html"))
-            {
-                StringBuilder builder = new StringBuilder();
-                builder.AppendLine($"From: {message.From}");
-                builder.AppendLine($"To: {message.To}");
-                builder.AppendLine($"BCC: {message.Bcc}");
-                builder.AppendLine($"Subject: {message.Subject}");
-                builder.AppendLine(message.Body);
-
-
-                byte[] info = new UTF8Encoding(true).GetBytes(builder.ToString());
-                fs.Write(info, 0, info.Length);
-
-                // writing data in bytes already
-                byte[] data = new byte[] { 0x0 };
-                fs.Write(data, 0, data.Length);
-            }
+            client.PickupDirectoryLocation = rootDirectory;
+            client.Send(message);
         }
     }
 }
